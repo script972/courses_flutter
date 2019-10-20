ddde4cd7151adec81ad97e0078909bbfcf9dd406
script972 <script972@gmail.com>
2019-10-20 23:05:05 +0300

    init commit

---
+++ lesson1/overview2(helloWorld)/android/app/src/main/java/ua/script972/hello_world/MainActivity.java
@@ -0,0 +1,13 @@
+package ua.script972.hello_world;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
